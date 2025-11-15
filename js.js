d34cf2e65816586f3b0e2ed63d7e50e73175ab01
Doszhan Daliev <doszhan.daliev@mail.ru>
2025-11-15 22:46:31 +0500

    Done?

--- js.js
+++ js.js
@@ -1,16 +1,26 @@
 window.onload = init;
 
-function showAnswer(eventObj){
-    let image = eventObj.target;
-
-    let name = image.id;
-    image.src = `${name}.jpg`
-}
-
 function init(){
     let images = document.getElementsByTagName("img");
     for(i = 0; i < images.length; i++){
-        images[i].onclick = showAnswer;
+        images[i].onmouseover = showAnswer;
+        images[i].onmouseout = reblur;
     }
 }
+
+function showAnswer(eventObj){
+    let image = eventObj.target;
+    let name = image.id;
+    image.src = `${name}.jpg`;
+}
+
+function reblur(eventObj){
+    let image = eventObj.target;
+    let name = image.id;
+    image.src = `${name}Blur.png`;
+}
+
+
+
+
 
